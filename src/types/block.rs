606e8a1a738c0ae4bda1a802fd399fa382bc15c9
sven <semirazov94@gmail.com>
2021-10-16 10:58:23 +0300

    define structure

--- src/types/block.rs
+++ src/types/block.rs
@@ -1,8 +1,46 @@
 use crate::types::{Hash, Transaction};
 
+#[derive(Default, Debug)]
 pub struct Block {
     nonce: u128,
     hash: Hash,
-    prev_hash: Hash,
+    prev_hash: Option<Hash>,
     transactions: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn new(prev_hash: Option<Hash>) -> Self {
+        Block {
+            prev_hash,
+            ..Default::default()
+        }
+    }
+
+    pub fn set_nonce(&mut self, nonce: u128) {
+        self.nonce = nonce;
+    }
+
+    pub fn add_transaction(&mut self, transaction: Transaction) {
+        self.transactions.push(transaction);
+    }
+
+    pub fn hash(&self) -> Hash {
+        String::new()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::TransactionData;
+
+    #[test]
+    fn test_creation() {
+        let mut block = Block::new(None);
+        let mut tx = Transaction::new(TransactionData::CreateAccount("alice".to_string()), None);
+        block.set_nonce(1);
+        block.add_transaction(tx);
+
+        dbg!(block);
+    }
 }
--- src/types/transaction.rs
+++ src/types/transaction.rs
@@ -1,15 +1,29 @@
 use crate::types::{AccountId, Balance, Timestamp};
 
+#[derive(Debug)]
 pub struct Transaction {
     nonce: u128,
     timestamp: Timestamp,
-    from: AccountId,
+    from: Option<AccountId>,
     data: TransactionData,
     signature: Option<String>,
 }
 
+#[derive(Debug)]
 pub enum TransactionData {
     CreateAccount(AccountId),
     MintInitialSupply { to: AccountId, amount: Balance },
     Transfer { to: AccountId, amount: Balance },
+}
+
+impl Transaction {
+    pub fn new(data: TransactionData, from: Option<AccountId>) -> Self {
+        Self {
+            nonce: 0,
+            timestamp: 0,
+            from,
+            data,
+            signature: None,
+        }
+    }
 }
